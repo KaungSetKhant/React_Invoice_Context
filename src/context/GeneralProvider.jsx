2f677a7d738422f81b4331b3bb184c69df7b9a6b
KaungSetKhant <kaungsetkhant5201@gmail.com>
2024-06-21 15:31:16 +0630

    react-invoice-context

---
+++ src/context/GeneralProvider.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from "react";
+
+export const GeneralContext = createContext();
+
+const GeneralProvider = ({ children }) => {
+    const [openDrawer, setOpenDrawer] = useState(false);
+
+  const handleDrawer = () => {
+    setOpenDrawer(!openDrawer);
+    // console.log("you click");
+  };
+  return (
+    <GeneralContext.Provider
+    value={{
+      openDrawer,
+      handleDrawer,
+    }}
+  >
+    {children}
+  </GeneralContext.Provider>
+  )
+}
+
+export default GeneralProvider
